52cd53c748d7530eea982acc91c455fd89c297eb
Your Name <you@example.com>
2021-02-15 10:46:29 +0900

    環境変数での設定ができるように変更

--- estcmd/startscript/startscript.sh
+++ estcmd/startscript/startscript.sh
@@ -6,6 +6,6 @@ cp --force /cron_configuration/cron_configuration /var/spool/cron/crontabs/root
 cat /var/spool/cron/crontabs/root
 echo 
 echo /== First estcmd gather ==/
-estcmd gather -cl -il ja -sd -cm /index /source
+${INITIAL_COMMAND} /index /source
 echo /== Cron Start ==/
 crond -l 2 -f
--- lighttpd/startscript/startscript.sh
+++ lighttpd/startscript/startscript.sh
@@ -2,5 +2,14 @@
 mkdir -p /www/public
 cp /usr/local/libexec/estseek.cgi /www/public
 chmod 755 /www/public/estseek.cgi 
-ln -s /configuration/estseek.* /www/public
+
+for f in /configuration/estseek.*.template
+do 
+    #echo $f
+    outputfile=${f%.template}
+    outputfile_basename=${outputfile##*/}
+    #echo $outputfile_basename
+    envsubst < $f > /www/public/$outputfile_basename
+done
+
 lighttpd -D -f /configuration/lighttpd.conf
